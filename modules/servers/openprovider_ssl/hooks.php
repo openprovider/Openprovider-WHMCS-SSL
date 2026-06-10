7ad90551b287d0f2412b556b5c4ed98cecf8fe8d
YumeChaan <chan.19@cse.mrt.ac.lk>
2026-06-10 09:39:36 +0530

    fix: copilot - use filter_var for with_config and fix wildcard SAN in hooks

--- modules/servers/openprovider_ssl/hooks.php
+++ modules/servers/openprovider_ssl/hooks.php
@@ -161,9 +161,9 @@ add_hook('AdminAreaHeadOutput', 1, function ($vars) {
                         "signature_hash_algorithm" => $dataArray['signature_hash_algorithm'],
                         "state" => $dataArray['state'],
                         "unit" => $dataArray['unit'],
-                        "with_config" => (bool)$dataArray['with_config'],
+                        "with_config" => filter_var($dataArray['with_config'], FILTER_VALIDATE_BOOLEAN),
                         "subject_alternative_name" => [
-                            "www.".$dataArray['common_name'],
+                            str_starts_with($dataArray['common_name'], '*.') ? $dataArray['common_name'] : 'www.' . $dataArray['common_name'],
                         ],
                     ];
                     $baseUrl = $helper->getBaseUrl();
